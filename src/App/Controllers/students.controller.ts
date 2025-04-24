505070f1955a201a563ab61d302e85d1b8c91861
nKhanGh <khang.nguyenkhanghuu14@hcmut.edu.vn>
2025-04-25 01:05:30 +0700

    fix crash in server

--- src/App/Controllers/students.controller.ts
+++ src/App/Controllers/students.controller.ts
@@ -1,31 +1,21 @@
-import { Request, Response } from "express";
-import pool from "@/Config/db.config"
+import { Request, Response } from 'express';
+import pool from '../../Config/db.config';
 
 class StudentController {
-
   static async getStudent(req: Request, res: Response) {
-    const [rows] = await pool.query("SELECT * FROM student");
+    const [rows] = await pool.query('SELECT * FROM student');
     res.status(200).json(rows);
   }
 
-  static async getStudentBySsn(req: Request, res: Response): Promise<void> {
-    
-  }
+  static async getStudentBySsn(req: Request, res: Response): Promise<void> {}
 
-  static async createStudent(req: Request, res: Response) {
-    
-  }
+  static async createStudent(req: Request, res: Response) {}
 
-  static async put(req: Request, res: Response): Promise<void> {
-    
-  }
+  static async put(req: Request, res: Response): Promise<void> {}
 
-  static async delete(req: Request, res: Response): Promise<void> {
-    
-  }
+  static async delete(req: Request, res: Response): Promise<void> {}
 
-  static async getNoRelative(req: Request, res: Response) {
-  }
-};
+  static async getNoRelative(req: Request, res: Response) {}
+}
 
 export default StudentController;
